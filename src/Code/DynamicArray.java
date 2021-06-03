c1bef475f172be89b23d284e947329d941022205
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-06-03 21:58:31 +0300

    Add DynamicArray

--- src/Code/DynamicArray.java
+++ src/Code/DynamicArray.java
@@ -1,9 +1,15 @@
 package Code;
 
 public class DynamicArray {
+public class DynamicArray <T> {
     public void throwException(){
     throw new ArithmeticException("Зачем ломаешь");
 
     }
-    
+    private final int DEFAULT_SIZE = 10;
+    private T[] array;
+
+    public DynamicArray(){
+        array = (T[])new Object[DEFAULT_SIZE];
+    }
 }
