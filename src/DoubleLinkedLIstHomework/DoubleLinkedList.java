9b06e103c0a1aa53abbee651650f01e21117a8d9
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-08-27 14:07:21 +0300

    Create DoubleLinkedList

---
+++ src/DoubleLinkedLIstHomework/DoubleLinkedList.java
@@ -0,0 +1,13 @@
+package DoubleLinkedLIstHomework;
+
+public class DoubleLinkedList <L> {
+
+    private DoubleLinkedList<L> head;
+    private DoubleLinkedList<L> tail;
+
+    public DoubleLinkedList() {
+
+        head = null;
+        tail = null;
+    }
+}
