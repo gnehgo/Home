e04299ea0f946546ae0bda1abef18e87859e8ff5
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-08-27 16:47:04 +0300

    AddFirts

--- src/DoubleLinkedLIstHomework/DoubleLinkedList.java
+++ src/DoubleLinkedLIstHomework/DoubleLinkedList.java
@@ -4,10 +4,21 @@ public class DoubleLinkedList <L> {
 
     private DoubleLinkedListElement head;
     private DoubleLinkedListElement tail;
+    private int size;
 
     public DoubleLinkedList() {
 
         head = null;
-        tail = null;
+        tail = null;}
+        public void addFirst(L data) {
+            DoubleLinkedListElement tmp = new DoubleLinkedListElement(data, head, null);
+            if(head != null ) {
+                head.prev = tmp;
+            }
+            head = tmp;
+            if(tail == null) {
+                tail = tmp;
+            }
+            size++;
+        }
     }
-}
