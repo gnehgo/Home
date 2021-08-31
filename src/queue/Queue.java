2b5f3cb17c36bc511581a7b36162fe4408729b54
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-08-31 15:03:02 +0300

    add getSize

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -5,6 +5,13 @@ import DoubleLinkedLIstHomework.DoubleLinkedListElement;
 
 public class Queue {
     DoubleLinkedList doublelist = new DoubleLinkedList<Integer>();
+
+    int size = 0;
+
+    public int getSize(){
+        return size;
+    }
     
+
 
 }
