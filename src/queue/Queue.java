0e12c344617bd1ccf4df5f8f5143ee0d8e1df662
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-08-31 14:54:56 +0300

    Update Queue.java

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -2,6 +2,9 @@ package queue;
 
 import DoubleLinkedLIstHomework.DoubleLinkedList;
 import DoubleLinkedLIstHomework.DoubleLinkedListElement;
+
 public class Queue {
+    DoubleLinkedList doublelist = new DoubleLinkedList<Integer>();
+    
 
 }
