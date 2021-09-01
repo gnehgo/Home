a2e5fb98eb70f2ef13d731a1fb36cfd7580e6b34
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-09-01 17:41:25 +0300

    Create QuadraticSortTest.java

---
+++ src/QuadraticSort/QuadraticSortTest.java
@@ -0,0 +1,9 @@
+package QuadraticSort;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+
+public class QuadraticSortTest  extends Assertions{
+    
+}
