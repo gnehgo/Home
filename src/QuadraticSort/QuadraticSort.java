a554ecaa3b45c632a14570192e77c58119e9109f
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-09-01 17:40:16 +0300

    Create QuadraticSort.java

---
+++ src/QuadraticSort/QuadraticSort.java
@@ -0,0 +1,24 @@
+package QuadraticSort;
+import java.util.Arrays;
+
+public class QuadraticSort {
+    public void sort(int[] data)
+    {
+        int temp;
+        boolean sorted = false;
+
+        while (!sorted) {
+
+            sorted = true;
+            for (int i = 0; i < data.length - 1; i++) {
+                if (data[i] > data[i+1]) {
+                    temp = data[i];
+                    data[i] = data[i+1];
+                    data[i+1] = temp;
+                    sorted = false;
+                }
+            }
+
+        }
+    }
+}
