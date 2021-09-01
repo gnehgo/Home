03f6be3e806de2811a4f91c213d95ddec87c3d30
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-09-01 11:56:45 +0300

    add Test

--- src/DoubleLinkedLIstHomework/DoubleLinkedListTest.java
+++ src/DoubleLinkedLIstHomework/DoubleLinkedListTest.java
@@ -1,6 +1,38 @@
 package DoubleLinkedLIstHomework;
+
        import  DoubleLinkedLIstHomework.DoubleLinkedList;
        import org.junit.jupiter.api.Test;
        import org.junit.jupiter.api.Assertions;
+
         public class DoubleLinkedListTest {
-}
+
+            @Test
+            public void testIsEmptyReturnsTrue() {
+                DoubleLinkedList<Integer> doublelist = new DoubleLinkedList<Integer>();
+                Assertions.assertTrue(doublelist.isEmpty());
+            }
+
+            @Test
+            public void testIsEmptySizeZero() {
+                DoubleLinkedList<Integer> doublelist = new DoubleLinkedList<Integer>();
+                Assertions.assertEquals(0, doublelist.size());
+            }
+            @Test
+            public void testAddHead() {
+                DoubleLinkedList<Integer> doublelist = new DoubleLinkedList<Integer>();
+                for (int i = 0; i < 5; i++) {
+                    doublelist.addHead(i);
+                }
+                Assertions.assertEquals(4, doublelist.getData(0));
+            }
+
+            @Test
+            public void testAddTail(){
+                DoubleLinkedList<Integer> doublelist = new DoubleLinkedList<Integer>();
+                for (int i = 0; i < 5; i++) {
+                    doublelist.addTail(i);
+                }
+                Assertions.assertEquals(3, doublelist.getData(3));
+            }
+
+            }
