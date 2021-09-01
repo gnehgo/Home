9763d07a5f1ae21dcac10f97a1ddb8b12f9629d9
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-09-01 11:53:58 +0300

    Create DoubleLinkedListTest

---
+++ src/DoubleLinkedLIstHomework/DoubleLinkedListTest.java
@@ -0,0 +1,6 @@
+package DoubleLinkedLIstHomework;
+       import  DoubleLinkedLIstHomework.DoubleLinkedList;
+       import org.junit.jupiter.api.Test;
+       import org.junit.jupiter.api.Assertions;
+        public class DoubleLinkedListTest {
+}
