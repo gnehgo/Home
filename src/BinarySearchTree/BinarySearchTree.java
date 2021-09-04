d5ede28a7309b17e486e5b7d20aa34494508f363
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-09-04 21:45:32 +0300

    add Insert

--- src/BinarySearchTree/BinarySearchTree.java
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -1,4 +1,42 @@
 package BinarySearchTree;
 
 public class BinarySearchTree {
+    Item root;
+
+    public BinarySearchTree() {
+        root = new Item();
+    }
+
+    public BinarySearchTree(String key) {
+        root = new Item(key);
+    }
+
+    public void insert(String key) {
+        if (root.key == null) {
+            root = new Item(key);
+            return;
+        }
+        insertRec(key, root);
+    }
+
+    private void insertRec(String key, Item node) {
+        
+        if (key.compareTo(node.key) < 0) {
+            if (node.leftChild != null) {
+                insertRec(key, node.leftChild);
+            } else {
+                node.leftChild = new Item(key);
+                node.leftChild.parent = node;
+            }
+        }
+
+        if (key.compareTo(node.key) >= 0) {
+            if (node.rightChild != null) {
+                insertRec(key, node.rightChild);
+            } else {
+                node.rightChild = new Item(key);
+                node.rightChild.parent = node;
+            }
+        }
+    }
 }
--- src/BinarySearchTree/Item.java
+++ src/BinarySearchTree/Item.java
@@ -1,4 +1,22 @@
 package BinarySearchTree;
 
 public class Item {
+    public String key;
+    Item leftChild;
+    Item rightChild;
+    Item parent;
+
+    public Item() {
+        key = null;
+        leftChild = null;
+        rightChild = null;
+        parent = null;
+    }
+
+    public Item(String key) {
+        this.key = key;
+        leftChild = null;
+        rightChild = null;
+        parent = null;
+    }
 }
