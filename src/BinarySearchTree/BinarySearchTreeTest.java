f1cc893f240946f09b601e2c30306f068a006127
gnehgo <79279203+gnehgo@users.noreply.github.com>
2021-09-04 23:16:14 +0300

    Create BinarySearchTreeTest.java

---
+++ src/BinarySearchTree/BinarySearchTreeTest.java
@@ -0,0 +1,10 @@
+package BinarySearchTree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BinarySearchTreeTest extends Assertions {
+
+
+}
